6f8fa4cd6b6c002e90bfdfbcf97f073b27b2a53b
benproe291 <98323905+benproe291@users.noreply.github.com>
2022-12-04 19:11:33 -0500

    Complete first 2 views of Genres

--- js/genres.js
+++ js/genres.js
@@ -3,14 +3,16 @@ thumbnails = document.querySelectorAll('.thumbnail')
 for (var index = 0; index < thumbnails.length; index++) {
   console.log('adding image listener index')
   console.log(thumbnails[index])
-  thumbnails[index].addEventListener('mouseenter', function () {
-    document.querySelector('#display').style.backgroundImage =
+  console.log(this)
+  thumbnails[index].addEventListener('click', function () {
+    document.querySelector('#displayImg').style.backgroundImage =
       "url('" + this.src + "')"
-    document.querySelector('#display').innerHTML = this.alt
+    document.querySelector('#imgName').innerHTML = this.alt
+    document.querySelector('#imgDesc').innerHTML = this.data-price
   })
-  thumbnails[index].addEventListener('mouseleave', function () {
-    document.querySelector('#display').style.backgroundImage = ''
-    document.querySelector('#display').innerHTML =
-      'Hover over an image below to display the image and the alt text.'
-  })
+  // thumbnails[index].addEventListener('mouseleave', function () {
+  //   document.querySelector('#displayImg').style.backgroundImage = 'none'
+  //   document.querySelector('#imgName').innerHTML =
+  //     'Hover over an image below to display the image and the alt text.'
+  // })
 }
